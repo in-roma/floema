589a6719d7e2abcd76371c644a06745eecf07295
Romain Grandjean <romain.grandjean@outlook.com>
2021-07-28 20:37:13 +0200

    webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,16 @@ const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev';
 
 const dirApp = path.join(__dirname, 'app');
 const dirAssets = path.join(__dirname, 'assets');
+const dirImages = path.join(__dirname, 'images');
 const dirStyles = path.join(__dirname, 'styles');
 const dirNode = 'node_modules';
+const dirVideos = path.join(__dirname, 'videos');
 
 module.exports = {
 	entry: [path.join(dirApp, 'index.js'), path.join(dirStyles, 'index.scss')],
-	resolve: { modules: [dirApp, dirAssets, dirStyles, dirNode] },
+	resolve: {
+		modules: [dirApp, dirAssets, dirImages, dirStyles, dirNode, dirVideos],
+	},
 	plugins: [
 		new webpack.DefinePlugin({ IS_DEVELOPMENT }),
 		new CopyWebpackPlugin({ patterns: [{ from: './assets', to: '' }] }),
@@ -42,7 +46,19 @@ module.exports = {
 					{
 						loader: 'css-loader',
 					},
+					{
+						loader: 'postcss-loader',
+					},
 				],
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
+				loader: 'file-loader',
+				options: {
+					name(file) {
+						return '[hash].[ext]';
+					},
+				},
 			},
 		],
 	},
