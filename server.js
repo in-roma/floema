9810a7f96c89df822c9d83786c56527fe97708bf
Romain Grandjean <romain.grandjean@outlook.com>
2021-08-16 22:39:40 +0200

    inmplementing prismic

--- server.js
+++ server.js
@@ -1,7 +1,38 @@
+require('dotenv').config();
+
+const Prismic = require('@prismicio/client');
+var PrismicDOM = require('prismic-dom');
+
 const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+
+const initApi = (req) => {
+	return Prismic.getApi(process.env.PRIMSIC_ENDPOINT, {
+		accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+		req,
+	});
+};
+
+const handleResolver = (doc) => {
+	// if (doc.type === 'page') {
+	// 	return '/page/' + doc.uid;
+	// } else if (doc.type === 'blog_post') {
+	// 	return '/blog/' + doc.uid;
+	// }
+
+	return '/';
+};
+
+app.use((req, res, next) => {
+	res.locals.ctx = {
+		endpoint: process.env.PRIMSIC_ENDPOINT,
+		linkResolver: handleResolver,
+	};
+	res.locals.PrismicDOM = PrismicDOM;
+	next();
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -11,6 +42,16 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
+	initApi(req).then((api) => {
+		api.query(Prismic.Predicates.at('document.type', 'about')).then(
+			(response) => {
+				const { results } = response;
+				const { about } = results;
+				console.log(about);
+				res.render('pages/about');
+			}
+		);
+	});
 	res.render('pages/about');
 });
 
